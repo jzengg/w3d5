2154a392ef4fbbd451d0068e6b98b16d52e84b6d
Jimmy Zeng <jzengg@gmail.com>
2015-10-16 11:34:25 -0400

    Add column getter

--- active_record_lite_skeleton/lib/01_sql_object.rb
+++ active_record_lite_skeleton/lib/01_sql_object.rb
@@ -5,7 +5,13 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    
+    columns = DBConnection.execute2(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    SQL
+    columns.first.map(&:to_sym)
   end
 
   def self.finalize!
