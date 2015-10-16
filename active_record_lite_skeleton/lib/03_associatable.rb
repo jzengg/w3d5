2bf237afceceda828c945884abf627aba72dea4e
Jimmy Zeng <jzengg@gmail.com>
2015-10-16 15:11:01 -0400

    Add model_class, table_name and defaults for belongs to and has many

--- active_record_lite_skeleton/lib/03_associatable.rb
+++ active_record_lite_skeleton/lib/03_associatable.rb
@@ -10,24 +10,49 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: "#{name.camelize}"
+    }
+    attributes = defaults.merge(options)
+
+    attributes.each do |key, value|
+      sendable = "#{key}=".to_sym
+      self.send(sendable, value)
+    end
+
+
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: "#{name.camelize.singularize}"
+    }
+
+    attributes = defaults.merge(options)
+
+    attributes.each do |key, value|
+      sendable = "#{key}=".to_sym
+      self.send(sendable, value)
+    end
+
   end
+
 end
 
 module Associatable
@@ -46,5 +71,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+    # Mixin Associatable here...
 end
